2a62fbce432cea99d6341b0ead7a6fc7a2437da1
tomomi suzuki <horadeverano@gmail.com>
2020-10-16 18:26:13 +0900

    完完成したかも

---
+++ src/controllers/IndexServlet.java
@@ -0,0 +1,68 @@
+package controllers;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("/index")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IndexServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EntityManager em = DBUtil.createEntityManager();
+
+		int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+        // 最大件数と開始位置を指定してメッセージを取得
+        List<Task> tasks = em.createNamedQuery("getAllMessages",Task.class)
+                                   .setFirstResult(15 * (page - 1))
+                                   .setMaxResults(15)
+                                   .getResultList();
+
+        // 全件数を取得
+        long messages_count = (long)em.createNamedQuery("getMessagesCount", Long.class)
+                                      .getSingleResult();
+
+        em.close();
+
+        request.setAttribute("tasks", tasks);
+        request.setAttribute("messages_count", messages_count);     // 全件数
+        request.setAttribute("page", page);
+	    if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
+
+	    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+	    rd.forward(request, response);
+    }
+
+}
---
+++ src/models/validators/MessageValidator.java
@@ -0,0 +1,30 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Task;
+
+public class MessageValidator {
+    // バリデーションを実行する
+    public static List<String> validate(Task t) {
+        List<String> errors = new ArrayList<String>();
+
+     String content_error = _validateContent(t.getContent());
+        if(!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+
+    // タスクの必須入力チェック
+    private static String _validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return "タスクを入力してください。";
+        }
+
+        return "";
+    }
+}
